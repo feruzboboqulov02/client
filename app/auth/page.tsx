a6ff285674e7ee52d0bb6bc27bc37d866b03ed14
feruzboboqulov02 <feruzboboqulov002@gmail.com>
2025-07-29 13:36:34 +0500

    auth page setup & icons

---
+++ app/auth/page.tsx
@@ -0,0 +1,8 @@
+import {FaTelegram} from 'react-icons/fa';
+const Page = () => {
+  return (<div className="container max-w-md w-full h-screen flex justify-center items-center flex-col space-y-4">
+    <FaTelegram className="text-6xl text-blue-500" />
+  </div>)
+}
+
+export default Page
